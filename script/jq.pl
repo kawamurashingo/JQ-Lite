8db3bbeea32716c84ed248585025de3fb9214226
Kawamura Shingo <36791900+kawamurashingo@users.noreply.github.com>
2025-03-29 05:44:48 +0900

    Update jq.pl

---
+++ script/jq.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use JSON::PP;
+use FindBin;
+use lib "$FindBin::Bin/../lib";
+use JQ::Lite;
+
+# クエリをコマンドライン引数から取得
+my $query = shift or die "Usage: $0 '.query'
+";
+
+# 標準入力からJSONを読み込む
+my $json_text = do { local $/; <STDIN> };
+
+# JQ::Lite を使ってクエリを実行
+my $jq = JQ::Lite->new;
+my @results = $jq->run_query($json_text, $query);
+
+# 出力
+for my $r (@results) {
+    if (ref $r eq '') {
+        print "$r\n";
+    } else {
+        print encode_json($r), "\n";
+    }
+}
