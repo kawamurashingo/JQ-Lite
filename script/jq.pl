d055b6a6b9d0a76130747207f909f61e6eb11b94
Kawamura Shingo <36791900+kawamurashingo@users.noreply.github.com>
2025-03-29 09:33:28 +0900

    Update jq.pl

--- script/jq.pl
+++ script/jq.pl
@@ -7,29 +7,18 @@ use FindBin;
 use lib "$FindBin::Bin/../lib";
 use JQ::Lite;
 
-# クエリをコマンドライン引数から取得
 my $query = shift or die "Usage: $0 '.query'\n";
-
-# 標準入力からJSONを読み込む
 my $json_text = do { local $/; <STDIN> };
 
-# JQ::Lite を使ってクエリを実行
 my $jq = JQ::Lite->new;
 my @results = $jq->run_query($json_text, $query);
 
-# JSONエンコーダー（整形用）
 my $pp = JSON::PP->new->utf8->canonical->pretty;
 
-# 出力
 for my $r (@results) {
     if (!defined $r) {
         print "null\n";
-    } elsif ($query =~ /^\s*\.\s*$/) {
-        # `.` のときは pretty print
-        print $pp->encode($r);
-    } elsif (ref $r eq '') {
-        print "$r\n";
     } else {
-        print encode_json($r), "\n";
+        print $pp->encode($r);  # ← すべて pretty print
     }
 }
