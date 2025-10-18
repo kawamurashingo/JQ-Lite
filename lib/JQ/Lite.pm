a6d6382f0a4ae571edcfe03ee6c3acc0fc7444ea
Kawamura Shingo <36791900+kawamurashingo@users.noreply.github.com>
2025-10-18 11:20:33 +0900

    Bump version from 1.09 to 1.10

--- lib/JQ/Lite.pm
+++ lib/JQ/Lite.pm
@@ -9,7 +9,7 @@ use JQ::Lite::Filters;
 use JQ::Lite::Parser;
 use JQ::Lite::Util ();
 
-our $VERSION = '1.09';
+our $VERSION = '1.10';
 
 sub new {
     my ($class, %opts) = @_;
@@ -57,7 +57,7 @@ JQ::Lite - A lightweight jq-like JSON query engine in Perl
 
 =head1 VERSION
 
-Version 1.09
+Version 1.10
 
 =head1 SYNOPSIS
 
