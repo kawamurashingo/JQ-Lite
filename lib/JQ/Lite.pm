bd9e5f956f1ef8fce0386ac4d1d89e14fa8afefa
Kawamura Shingo <36791900+kawamurashingo@users.noreply.github.com>
2025-12-21 06:55:19 +0900

    Downgrade version from 1.54 to 1.53

--- lib/JQ/Lite.pm
+++ lib/JQ/Lite.pm
@@ -9,7 +9,7 @@ use JQ::Lite::Filters;
 use JQ::Lite::Parser;
 use JQ::Lite::Util ();
 
-our $VERSION = '1.54';
+our $VERSION = '1.53';
 
 sub new {
     my ($class, %opts) = @_;
@@ -60,7 +60,7 @@ JQ::Lite - A lightweight jq-like JSON query engine in Perl
 
 =head1 VERSION
 
-Version 1.54
+Version 1.53
 
 =head1 SYNOPSIS
 
