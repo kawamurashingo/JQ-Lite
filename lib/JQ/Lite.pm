37ce3b08f227e58b9a1d9d9ad3505d1f88441bce
Kawamura Shingo <36791900+kawamurashingo@users.noreply.github.com>
2026-01-10 19:40:44 +0900

    Bump version to 2.00

--- lib/JQ/Lite.pm
+++ lib/JQ/Lite.pm
@@ -9,7 +9,7 @@ use JQ::Lite::Filters;
 use JQ::Lite::Parser;
 use JQ::Lite::Util ();
 
-our $VERSION = '1.99';
+our $VERSION = '2.00';
 
 sub new {
     my ($class, %opts) = @_;
@@ -60,7 +60,7 @@ JQ::Lite - jq-compatible JSON query engine in pure Perl (no external binaries)
 
 =head1 VERSION
 
-Version 1.99
+Version 2.00
 
 =head1 SYNOPSIS
 
